38737b4e2803a7e20680acef80b992e3ac2b72b8
gcogoh <*****@users.noreply.github.com>
2021-10-26 18:37:58 +0900

    add usage of std::vector::insert instead of push_back

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,9 @@
+/**
+ * @file helloworld.cpp
+ * @brief VSCode template
+ * @author gcogoh@users.noreply.github.com
+ * @date 2020/08/08 create
+ */
 #include <iostream>
 #include <vector>
 #include <string>
@@ -9,11 +15,13 @@ int main(int argc, char** argv)
 {
 	vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!", "by CMake"};
 	Object obj;
-	obj.AddString("built");
-	obj.AddString("program.");
+	for (const auto& word : {"built", "program."})
+	{
+		obj.AddString(word);
+	}
 	obj.DisplayVectorContents();
-	msg.push_back(obj.GetCopyOfVector().at(0));
-	msg.push_back(obj.GetCopyOfVector().at(1));
+	auto objStr = obj.GetCopyOfVector();	// to reserve same vector& of GetCopyOfVector()
+	msg.insert(msg.end(), objStr.begin(), objStr.end());
 
 	for (const auto& word : msg)
 	{
